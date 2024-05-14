efb14f5b492287dabbc52673b6de4b7a7181fa30
ScryterX <josielcardosomercio@gmail.com>
2024-05-14 14:01:36 -0300

    feat: add promo banner to homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import CategoryList from "./_components/Category-list";
 import Header from "./_components/Header";
 import Search from "./_components/Search";
@@ -11,6 +12,18 @@ export default function Home() {
       </div>
       <div className="pt-6">
         <CategoryList />
+      </div>
+
+      <div className="px-5 pt-6">
+        <Image
+          src={"/banner-promo-01.png"}
+          alt="O melhor churrasco que você já comeu"
+          width={0}
+          height={0}
+          className="h-auto w-full rounded-lg object-contain"
+          sizes="100vw"
+          quality={100}
+        />
       </div>
     </div>
   );
