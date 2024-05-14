2d7d07830ba18681020b31557d7c3ac2ea313f27
ScryterX <josielcardosomercio@gmail.com>
2024-05-14 13:21:28 -0300

    feat: add initial search component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
 import Header from "./_components/Header";
+import Search from "./_components/Search";
 
 export default function Home() {
   return (
     <div>
       <Header />
+      <div className="px-5 pt-6">
+        <Search />
+      </div>
     </div>
   );
 }
