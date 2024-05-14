0924bbb55b9e1a59e64484c2ea0d78d8f8752169
ScryterX <josielcardosomercio@gmail.com>
2024-05-14 16:03:51 -0300

    fix: adjust category height component

--- app/_components/Category-list.tsx
+++ app/_components/Category-list.tsx
@@ -5,7 +5,7 @@ const CategoryList = async () => {
   const categories = await db.category.findMany({});
 
   return (
-    <div className="flex w-full min-w-[180px] gap-6 overflow-x-scroll pb-5 [&::-webkit-scrollbar]:hidden">
+    <div className="flex w-full min-w-[180px] gap-6 overflow-x-scroll pb-3 [&::-webkit-scrollbar]:hidden">
       {categories.map((category) => (
         <CategoryItem key={category.id} category={category} />
       ))}
