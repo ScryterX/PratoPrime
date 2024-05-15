72e909cf438206a196b1fcf66a3f28a5cd69a2a7
ScryterX <josielcardosomercio@gmail.com>
2024-05-15 20:58:22 -0300

    feat: add product page

---
+++ app/products/[id]/page.tsx
@@ -0,0 +1,42 @@
+import { db } from "@/app/_lib/prisma";
+
+import { notFound } from "next/navigation";
+import ProductImage from "./_components/product-image";
+import ProductDetails from "./_components/product-details";
+
+interface ProductPageProp {
+  params: {
+    id: string;
+  };
+}
+const productPage = async ({ params: { id } }: ProductPageProp) => {
+  const product = await db.product.findUnique({
+    where: {
+      id,
+    },
+    include: {
+      restaurant: true,
+    },
+  });
+
+  const juices = await db.product.findMany({
+    where: {
+      category: {
+        name: "Sucos",
+      },
+    },
+    include: {
+      restaurant: true,
+    },
+  });
+  if (!product) return notFound();
+
+  return (
+    <div>
+      <ProductImage product={product} />
+      <ProductDetails product={product} complementaryProducts={juices} />
+    </div>
+  );
+};
+
+export default productPage;
