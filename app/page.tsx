764d2582feb2e699b24ec06de77dc746dc35fb3a
ScryterX <josielcardosomercio@gmail.com>
2024-05-16 16:18:33 -0300

    feat: add recommended products page

--- app/page.tsx
+++ app/page.tsx
@@ -46,13 +46,15 @@ const Home = async () => {
       <div className="space-y-4 pt-6">
         <div className="flex items-center justify-between px-5 ">
           <h2 className="font-semibold">Pedidos recomendados</h2>
-          <Button
-            className="h-fit p-0 text-primary hover:bg-transparent"
-            variant={"ghost"}
-          >
-            Ver todos
-            <ChevronRightIcon size={16} />
-          </Button>
+          <Link href={"/products/recommended"}>
+            <Button
+              className="h-fit p-0 text-primary hover:bg-transparent"
+              variant={"ghost"}
+            >
+              Ver todos
+              <ChevronRightIcon size={16} />
+            </Button>
+          </Link>
         </div>
         <ProductList products={products} />
       </div>
